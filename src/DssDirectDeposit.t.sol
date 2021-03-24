839df5acc98cb7648752b86b087dbe383fbeb8b8
Sam MacPherson <xylom44@gmail.com>
2021-03-24 19:46:17 -0400

    PoC with actual interest rate calc

--- src/DssDirectDeposit.t.sol
+++ src/DssDirectDeposit.t.sol
@@ -30,12 +30,23 @@ interface LendingPoolLike {
     );
 }
 
+interface InterestRateStrategyLike {
+    function OPTIMAL_UTILIZATION_RATE() external view returns (uint256);
+    function EXCESS_UTILIZATION_RATE() external view returns (uint256);
+    function variableRateSlope1() external view returns (uint256);
+    function variableRateSlope2() external view returns (uint256);
+    function baseVariableBorrowRate() external view returns (uint256);
+}
+
 contract DssDirectDepositTest is DSTest {
+
+    uint256 constant RAY = 10 ** 27;
 
     Hevm hevm;
 
     DssDirectDeposit deposit;
     LendingPoolLike pool;
+    InterestRateStrategyLike interestStrategy;
     DaiAbstract dai;
     DSTokenAbstract adai;
 
@@ -46,6 +57,7 @@ contract DssDirectDepositTest is DSTest {
         pool = LendingPoolLike(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
         adai = DSTokenAbstract(0x028171bCA77440897B824Ca71D1c56caC55b68A3);
         dai = DaiAbstract(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        interestStrategy = InterestRateStrategyLike(0xfffE32106A68aA3eD39CcCE673B646423EEaB62a);
 
         // Mint ourselves 100B DAI
         giveTokens(DSTokenAbstract(address(dai)), 100_000_000_000 ether);
@@ -85,11 +97,26 @@ contract DssDirectDepositTest is DSTest {
         assertTrue(false);
     }
 
+    function calculateLiquidityRequiredForTargetInterestRate(uint256 interestRate) public returns (uint256) {
+        // Do inverse calc
+        uint256 supplyAmount = adai.totalSupply();
+        uint256 borrowAmount = supplyAmount - dai.balanceOf(address(adai));
+        log_named_decimal_uint("supplyAmount", supplyAmount, 18);
+        log_named_decimal_uint("borrowAmount", borrowAmount, 18);
+        uint256 targetUtil = (interestRate - interestStrategy.baseVariableBorrowRate()) * RAY / interestStrategy.variableRateSlope1();
+        log_named_decimal_uint("targetUtil", targetUtil, 27);
+        uint256 targetSupply = borrowAmount * RAY / targetUtil;
+        log_named_decimal_uint("targetSupply", targetSupply, 18);
+        return targetSupply - supplyAmount;
+    }
+
     function test_set_aave_interest_rate() public {
         (,,,, uint256 borrowRate,,,,,,,) = pool.getReserveData(address(dai));
         log_named_decimal_uint("origBorrowRate", borrowRate, 27);
 
-        pool.deposit(address(dai), 1_000_000 ether, address(this), 0);
+        uint256 deltaSupply = calculateLiquidityRequiredForTargetInterestRate(3 * RAY / 100);
+
+        pool.deposit(address(dai), deltaSupply, address(this), 0);
 
         (,,,, borrowRate,,,,,,,) = pool.getReserveData(address(dai));
         log_named_decimal_uint("newBorrowRate", borrowRate, 27);
