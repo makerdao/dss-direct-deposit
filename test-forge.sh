d936823da754927134b633756951bfee9161068b
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-25 14:54:45 -0700

    add forge test option

---
+++ test-forge.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+
+    case "$KEY" in
+            match)  MATCH="$VALUE" ;;
+            block)  BLOCK="$VALUE" ;;     
+            *)   
+    esac
+done
+
+if [[ -z "$MATCH" && -z "$BLOCK" ]]; then
+    forge test --fork-url "$ETH_RPC_URL" --force
+elif [[ -z "$BLOCK" ]]; then
+    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force
+elif [[ -z "$MATCH" ]]; then
+    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" --force
+else
+    forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force
+fi
