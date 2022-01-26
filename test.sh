4f9e65ac31775a390f6b7a93e487a721c3fc2b97
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-01-26 11:13:58 -0700

    Add forge (#27)
    * allow non-optimizer tests
    
    * add forge test option
    
    * add cache to gitignore
    
    * add line break
    
    * file clean up
    
    * add verbosity to regular tests
    
    Co-authored-by: Nazzareno Massari <nazzareno@nazzarenomassari.com>
    
    Co-authored-by: Nazzareno Massari <nazzareno@nazzarenomassari.com>

--- test.sh
+++ test.sh
@@ -3,11 +3,23 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive" ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1; }
 
-export DAPP_BUILD_OPTIMIZE=1
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+
+    case "$KEY" in
+            match)      MATCH="$VALUE" ;;
+            optimizer)  OPTIMIZER="$VALUE" ;;     
+            *)   
+    esac
+done
+
+export DAPP_BUILD_OPTIMIZE="$OPTIMIZER"
 export DAPP_BUILD_OPTIMIZE_RUNS=200
 
 if [[ -z "$1" ]]; then
   dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" -v
 else
-  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$1" -vv
+  dapp --use solc:0.6.12 test --rpc-url="$ETH_RPC_URL" --match "$MATCH" -vv
 fi
