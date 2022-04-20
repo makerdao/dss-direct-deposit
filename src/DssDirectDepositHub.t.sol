ddb68703fa766ae442dd072beb299a64fc2275fb
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-04-20 16:19:59 -0300

    Add comments

--- src/DssDirectDepositHub.t.sol
+++ src/DssDirectDepositHub.t.sol
@@ -354,13 +354,13 @@ contract DssDirectDepositHubTest is DSTest {
         _windSystem();
 
         // Set ilk line below current debt
-        d3mTestPlan.file("targetAssets", 55 * WAD);
+        d3mTestPlan.file("targetAssets", 55 * WAD); // Increasing target in 5 WAD
         vat.file(ilk, "line", 45 * RAD);
         directDepositHub.exec(ilk);
 
         // Ensure we unwound our position to debt ceiling
         (uint256 ink, uint256 art) = vat.urns(ilk, address(d3mTestPool));
-        assertEq(ink, 45 * WAD);
+        assertEq(ink, 45 * WAD); // Instead of 5 WAD more results in 5 WAD less due debt ceiling
         assertEq(art, 45 * WAD);
         // Make sure unwind calls accrued
         assertTrue(d3mTestPool.accrued());
@@ -370,7 +370,7 @@ contract DssDirectDepositHubTest is DSTest {
         _windSystem();
 
         // Set ilk line below current debt
-        d3mTestPlan.file("targetAssets", 55 * WAD);
+        d3mTestPlan.file("targetAssets", 55 * WAD); // Increasing target in 5 WAD
         vat.file("Line", vat.debt() - 5 * RAD);
         directDepositHub.exec(ilk);
 
