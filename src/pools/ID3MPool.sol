ec87b8d33399763790d29da10b705deee2b70e09
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-05-10 09:58:34 -0600

    add Natspec comments to base Pool, Plan and Hub contracts (#43)
    * add docs for Hub
    
    * add comments to plan interface
    
    * fix ilk cage docs
    
    * add pool docs
    
    * comment clean up
    
    * fix grammar
    
    Co-authored-by: talbaneth <talbaneth@gmail.com>
    
    * spelling
    
    Co-authored-by: talbaneth <talbaneth@gmail.com>
    
    * unify wrapping point and punctuation
    
    * clarify plan doc
    
    * pr comments
    
    * contract -> interface
    
    * update comments
    
    Co-authored-by: talbaneth <talbaneth@gmail.com>
    
    Co-authored-by: talbaneth <talbaneth@gmail.com>

--- src/pools/ID3MPool.sol
+++ src/pools/ID3MPool.sol
@@ -16,15 +16,89 @@
 
 pragma solidity 0.6.12;
 
+/**
+    @title D3M Pool Interface
+    @notice Pool contracts are contracts that the Hub uses to standardize
+    interactions with external Pools.
+    @dev Implementing contracts will hold any balance provided by the external
+    pool as well as the balance in the Vat. This interface aims to use EIP-4626
+    guidelines for assets/shares/maxWithdraw etc.
+*/
 interface ID3MPool {
+    /**
+        @notice Deposit assets (Dai) in the external pool.
+        @dev If the external pool requires a different amount to be passed in, the
+        conversion should occur here as the Hub passes Dai [wad] amounts.
+        msg.sender must be authorized.
+        @param amt amount in asset (Dai) terms that we want to deposit
+    */
     function deposit(uint256 amt) external;
+
+    /**
+        @notice Withdraw assets (Dai) from the external pool.
+        @dev If the external pool requires a different amount to be passed in
+        the conversion should occur here as the Hub passes Dai [wad] amounts.
+        msg.sender must be authorized.
+        @param amt amount in asset (Dai) terms that we want to withdraw
+    */
     function withdraw(uint256 amt) external;
+
+     /**
+        @notice Transfer shares.
+        @dev If the external pool/shares contract requires a different amount be
+        passed in the conversion should occur here as the Hub passes Gem [wad]
+        amounts. msg.sender must be authorized.
+        @param dst address that should receive the shares
+        @param amt amount in Gem terms that we want to withdraw
+        @return bool whether the transfer was successful per ERC-20 standard
+    */
     function transfer(address dst, uint256 amt) external returns (bool);
+
+    /**
+        @notice Transfer all shares from this pool.
+        @dev msg.sender must be authorized.
+        @param dst address that should receive the shares.
+        @return bool whether the transfer was successful per ERC-20 standard
+    */
     function transferAll(address dst) external returns (bool);
+
+    /// @notice Some external pools require manually accruing fees/interest
     function accrueIfNeeded() external;
+
+    /**
+        @notice Balance of assets this pool "owns".
+        @dev This could be greater than the amount the pool can withdraw due to
+        lack of liquidity.
+        @return uint256 number of assets in Dai [wad]
+    */
     function assetBalance() external view returns (uint256);
+
+    /**
+        @notice Maximum number of assets the pool could deposit at present.
+        @return uint256 number of assets in Dai [wad]
+    */
     function maxDeposit() external view returns (uint256);
+
+    /**
+        @notice Maximum number of assets the pool could withdraw at present.
+        @return uint256 number of assets in Dai [wad]
+    */
     function maxWithdraw() external view returns (uint256);
+
+    /**
+        @notice Used to recover any ERC-20 accidentally sent to the pool.
+        --- YOU SHOULD NOT SEND ERC-20 DIRECTLY TO THIS CONTRACT ---
+        The presence of this function does not convey any right to recover tokens
+        sent to this contract. Maker Governance must evaluate and perform this
+        action at its sole discretion.
+        @dev msg.sender must be authorized.
+        @param token address of the ERC-20 token that will be transferred
+        @param dst address that should receive the shares
+        @param amt amount in Gem terms that we want to withdraw
+        @return bool whether the transfer was successful per ERC-20 standard
+    */
     function recoverTokens(address token, address dst, uint256 amt) external returns (bool);
+
+    /// @notice Reports whether the plan is active
     function active() external view returns (bool);
 }
