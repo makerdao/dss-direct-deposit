ed092a9b1876b2549342ad0f2c5d134ddd2fd365
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-05-12 16:23:02 -0600

    remove url passing

--- test-forge-blocks.sh
+++ test-forge-blocks.sh
@@ -1,17 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-for ARGUMENT in "$@"
-do
-    KEY=$(echo $ARGUMENT | cut -f1 -d=)
-    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
-
-    case "$KEY" in
-            url) URL="$VALUE" ;;
-            *)
-    esac
-done
-
 BLOCK=$(cast block-number)
 START=$((BLOCK-50))
 echo "Checking from $START to $BLOCK"
@@ -19,5 +8,5 @@ echo "Checking from $START to $BLOCK"
 for i in $( eval echo {$START..$BLOCK} )
 do
     echo "================================ $i ================================"
-    forge test --fork-url "$URL" --fork-block-number "$i" -v --force
+    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$i" -v --force
 done
