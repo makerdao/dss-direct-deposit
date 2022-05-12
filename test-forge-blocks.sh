8b45459fbaa7aed66054061e0541ac35ea8eab3c
Chris Smith <1408372+iamchrissmith@users.noreply.github.com>
2022-05-12 16:04:35 -0600

    add block tests to look at last 100 blocks

---
+++ test-forge-blocks.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)
+
+    case "$KEY" in
+            url) URL="$VALUE" ;;
+            *)
+    esac
+done
+
+BLOCK=$(cast block-number)
+START=$((BLOCK-50))
+echo "Checking from $START to $BLOCK"
+
+for i in $( eval echo {$START..$BLOCK} )
+do
+    echo "================================ $i ================================"
+    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$i" -vvv
+done
