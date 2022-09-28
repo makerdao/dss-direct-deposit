4305e2f9fe0dd177b682106ae333d2cdd493b165
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-09-28 12:40:42 -0300

    Enforce solc 0.8.14 in make test and use it for CI

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ do
     VALUE=$(echo $ARGUMENT | cut -f2 -d=)
 
     case "$KEY" in
+            solc)            SOLC="$VALUE" ;;
             match)           MATCH="$VALUE" ;;
             match-test)      MATCH_TEST="$VALUE" ;;
             match-contract)  MATCH_CONTRACT="$VALUE" ;;
@@ -18,25 +19,25 @@ do
 done
 
 if [[ -z "$MATCH" && -z "$BLOCK" && -z "$MATCH_TEST" && -z "$MATCH_CONTRACT" ]]; then
-    forge test --fork-url "$ETH_RPC_URL" -vv --force
+    forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" -vv --force
 elif [[ -z "$MATCH" && -z "$MATCH_TEST" && -z "$MATCH_CONTRACT" ]]; then
-    forge test --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" -vv --force
+    forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --fork-block-number "$BLOCK" -vv --force
 else
     if [[ -n "$BLOCK" ]]; then
         if [[ -n "$MATCH" ]]; then
-            forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match "$MATCH" --fork-block-number "$BLOCK" -vvv --force
         elif [[ -n "$MATCH_TEST" ]]; then
-            forge test --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" --fork-block-number "$BLOCK" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" --fork-block-number "$BLOCK" -vvv --force
         else
-            forge test --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" --fork-block-number "$BLOCK" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" --fork-block-number "$BLOCK" -vvv --force
         fi
     else
         if [[ -n "$MATCH" ]]; then
-            forge test --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match "$MATCH" -vvv --force
         elif [[ -n "$MATCH_TEST" ]]; then
-            forge test --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match-test "$MATCH_TEST" -vvv --force
         else
-            forge test --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" -vvv --force
+            forge test --use "$SOLC" --fork-url "$ETH_RPC_URL" --match-contract "$MATCH_CONTRACT" -vvv --force
         fi
     fi
 fi
