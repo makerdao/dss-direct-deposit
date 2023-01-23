c029442afcca21faf7da95adb06ab9e28b0c5487
Sam MacPherson <xylom44@gmail.com>
2023-01-23 15:56:30 -0600

    verify by default

--- deploy-core.sh
+++ deploy-core.sh
@@ -18,4 +18,4 @@ do
 done
 
 rm -f out/contract-exports.env
-forge script script/D3MCoreDeploy.s.sol:D3MCoreDeployScript --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
+forge script script/D3MCoreDeploy.s.sol:D3MCoreDeployScript --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast --verify
--- deploy.sh
+++ deploy.sh
@@ -18,4 +18,4 @@ do
 done
 
 rm -f out/contract-exports.env
-forge script script/D3MDeploy.s.sol:D3MDeployScript --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast
+forge script script/D3MDeploy.s.sol:D3MDeployScript --use solc:0.8.14 --rpc-url $ETH_RPC_URL --sender $ETH_FROM --broadcast --verify
