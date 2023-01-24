bece879fbc1466965ba2a851278ace1dfac09915
Sam MacPherson <xylom44@gmail.com>
2023-01-24 11:00:48 -0600

    Update deploy-core.sh
    Co-authored-by: Gonzalo Balabasquer <gbalabasquer@gmail.com>

--- deploy-core.sh
+++ deploy-core.sh
@@ -6,7 +6,7 @@ set -e
         echo "Please set FOUNDRY_ROOT_CHAINID (1 or 5) or ETH_RPC_URL";
         exit 1;
     }
-    export FOUNDRY_ROOT_CHAINID="$(cast chain-id)"
+    FOUNDRY_ROOT_CHAINID="$(cast chain-id)"
 }
 [[ "$FOUNDRY_ROOT_CHAINID" == "1" ]] || [[ "$FOUNDRY_ROOT_CHAINID" == "5" ]] || {
     echo "Invalid chainid of $FOUNDRY_ROOT_CHAINID. Please set your forking environment via ETH_RPC_URL or manually by defining FOUNDRY_ROOT_CHAINID (1 or 5)."
